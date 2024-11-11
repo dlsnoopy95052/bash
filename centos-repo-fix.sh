a4737cb2b4bba33a89db42a96fd35f595d671cfe
dlsnoopy95052 <32687526+dlsnoopy95052@users.noreply.github.com>
2024-11-10 18:19:34 -0800

    Update centos-repo-fix.sh

--- centos-repo-fix.sh
+++ centos-repo-fix.sh
@@ -2,8 +2,9 @@
 
 for file in $1/*; do
     if [[ -f $file ]]; then
-        # Add a # to lines with "a=b.c.d"
+        # Add a # to lines with "mirrorlist=http..."
         sed -i '/^mirrorlist=http*/s/^/#/' "$file"
+        # Remove a # from baseurl and change mirror... to vault...
         sed -i 's/^#baseurl=http:\/\/mirror/baseurl=http:\/\/vault/' "$file"
     fi
 done
